4ce87fcc033a5f1c15c726adc0c63531967de5f8
user <Maximmolchanov2004@gmail.com>
2025-11-03 22:15:38 +0200

    Lab2

--- src/main/java/edu/ntudp/Molchanov/L0/Main.java
+++ src/main/java/edu/ntudp/Molchanov/L0/Main.java
@@ -3,6 +3,6 @@ package edu.ntudp.Molchanov.L0;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello world");
+        System.out.printf("Hello world ");
     }
 }
